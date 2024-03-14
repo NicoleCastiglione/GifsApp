3537d562a477a201f57e054b951e4d86a0769cec
Nicole Castiglione <nicolecastiglione0@gmail.com>
2024-03-14 03:56:21 -0300

    bug fixed

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=H2Ta2IIlQKageU2GvE28EG1Wg9Fod2cw&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=H2Ta2IIlQKageU2GvE28EG1Wg9Fod2cw&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
